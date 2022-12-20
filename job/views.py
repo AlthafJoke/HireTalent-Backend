a2c4ff013e1c668654347dd903534c59a6d35940
Althafav <61247025+althafav@users.noreply.github.com>
2022-12-20 08:31:50 +0530

    candidates count added

--- job/views.py
+++ job/views.py
@@ -19,7 +19,7 @@ def getAllJobs(request):
     count = filterset.qs.count()
     
     #pagination
-    resPerPage = 2
+    resPerPage = 4
     
     paginator = PageNumberPagination()
     
@@ -36,9 +36,11 @@ def getAllJobs(request):
 @api_view(['GET'])
 def getJob(request, pk):
     job = get_object_or_404(Job, id=pk)
+    
+    candidates = job.candidatesapplied_set.all().count()
     serializer = JobSerializer(job, many=False)
     
-    return Response(serializer.data)
+    return Response({'job': serializer.data, 'candidates': candidates})
 
 
 @api_view(['POST'])
