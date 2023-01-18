87d0352570c857c1ea239a48896d677860482f5c
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-18 13:30:22 +0530

    rm staticfilesdir from settings.py

--- config/settings.py
+++ config/settings.py
@@ -188,9 +188,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static/"),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
