b87b3284d15e9e36ba9dc46983ef0d4bd02aafe2
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-19 19:06:27 +0530

    rm static dir and static url

--- config/settings.py
+++ config/settings.py
@@ -186,11 +186,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT =  BASE_DIR / 'staticfile'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static/"),
+
+# )
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(file), 'static')) 
+# STATICFILES_DIRS = (os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
