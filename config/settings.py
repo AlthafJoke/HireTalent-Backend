df8422f7c2815feffa5ec48e9a02fc2ab934a36d
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-19 19:16:18 +0530

    media url added

--- config/settings.py
+++ config/settings.py
@@ -192,9 +192,9 @@ STATIC_ROOT =  BASE_DIR / 'staticfile'
 #     os.path.join(BASE_DIR, "staticfile/"),
 # )
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(file), 'static')) 
-# STATICFILES_DIRS = (os.path.join('static'), )
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
