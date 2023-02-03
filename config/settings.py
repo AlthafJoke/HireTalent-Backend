b4c75718826db12e2635690acaf738d0f547034b
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-03 18:20:09 +0530

    cores added

--- config/settings.py
+++ config/settings.py
@@ -189,6 +189,7 @@ CORS_ALLOWED_ORIGINS = [
   
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://hire-talent-frontend.vercel.app/"
     # "https://hire-talent-frontend-rkeo.vercel.app/"
 ]
 
